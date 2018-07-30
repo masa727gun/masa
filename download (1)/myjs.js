66f1061592584d5099a878ba573465c460f2947a
g.masaaki727 <g.masaaki727@gmail.com>
2018-07-29 20:57:11 -0400

    message

---
+++ download (1)/myjs.js
@@ -0,0 +1,7 @@
+alert(`Hello,masaaki!`);
+//alert(`Hello,Masaaki!`);
+//alert(1+1);
+alert(Math.sqrt(1+1));
+alert(2+3);
+alert(1/0);
+alert(0/0);
---
+++ download/ex06-7.js
@@ -0,0 +1,7 @@
+var who=`world`;
+function sayhello11(){
+ alert(`Hello,`+who+`!`);
+}
+function someone(x){
+who=x;
+}
